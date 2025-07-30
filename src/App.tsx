677aa7f87ea8917e4d257e8ee4772640531ec2cc
Mohammed <GM+mami186@users.noreply.github.com>
2025-07-30 22:28:19 +0300

    main window works but with list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Navbar from "./Components/Navbar"
-
+import MainContent from "./Components/MainContent"
 const App = () => {
   return (
     <div className="w-full">
       <Navbar />
       
-
+      <MainContent></MainContent>
     </div>
   )
 }
--- src/Components/MainContent.tsx
+++ src/Components/MainContent.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react"
+import apiClient from "../services/api-client"
+
 interface Props{
-    
+    count: number
+    result:Game[]
+}
+
+interface Game{
+    id: number
+    name: string
+    rating: number
 }
 const MainContent = () => {
+    const [games, setgames]= useState<Game[]>([])
+    const [error , seterror] = useState('')
+    const [isLoading, setisLoading] = useState(false)
+
+    useEffect(()=>{
+        setisLoading(true)
+        apiClient.get('/games')
+        .then((res)=> setgames(res.data.results))
+        .catch((err)=> seterror(err.message))
+        .finally(()=>setisLoading(false))
+    })
   return (
-    <div>MainContent</div>
+    <>{error && <p>{error}</p>}
+    {isLoading && <div className="spinner-border"></div>}
+    <ul>
+        {games.map((game)=>(
+            <li key={game.id}>{game.name}</li>
+        ))}
+    </ul>
+    </>
   )
 }
 
