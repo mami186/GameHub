18183722363e31c8f757f2979179252aeb34225f
Mohammed <GM+mami186@users.noreply.github.com>
2025-08-13 12:37:27 +0300

    impliment infinite scrolling

--- src/Components/MainContent.tsx
+++ src/Components/MainContent.tsx
@@ -4,55 +4,62 @@ import LoadingSkeleton from "./LoadingSkeleton";
 import type { GameQuery } from "../App";
 import PlatformIcons from "./PlatformIcons";
 import * as React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
 	gameQuery: GameQuery;
 }
 
 const MainContent = ({ gameQuery }: Props) => {
-	const { data, error, isLoading, isFetching, fetchNextPage } =
+	const { data, error, isLoading, fetchNextPage ,hasNextPage} =
 		useGames(gameQuery);
 	const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	const gameCount = data.pages.reduce(
+		(total, page) => total + page.results.length,
+		0
+	);
 
 	return (
 		<>
 			{error && <p>{error.message}</p>}
-			<div className="flex flex-wrap justify-evenly px-2 sm:px-0 ">
-				{isLoading &&
-					skeletons.map((skeleton) => <LoadingSkeleton key={skeleton} />)}
-				{data?.pages.map((page, index) => (
-					<React.Fragment key={index}>
-						{page.results.map((game) => (
-							<div
-								className={
-									"flex flex-col flex-auto  sm:w-72 sm:max-w-96  rounded-lg overflow-hidden shadow-2xl  m-1 mb-2 "
-								}
-								key={game.id}
-							>
-								<img src={MediaResize(game.background_image)} alt="Image" />
-								<div className="flex flex-col">
-									<div className="flex items-center justify-between px-3 py-2 h-auto">
-										<div>{game.name}</div>
-										<div className="bg-gray-500 opacity-70 rounded p-0 text-center">
-											{game.rating}
+			<InfiniteScroll dataLength={gameCount}
+			hasMore={hasNextPage}
+			next={() => fetchNextPage()}
+			loader={<p>Loading...</p>}
+			>
+
+				<div className="flex flex-wrap justify-evenly px-2 sm:px-0 ">
+					{isLoading &&
+						skeletons.map((skeleton) => <LoadingSkeleton key={skeleton} />)}
+					{data?.pages.map((page, index) => (
+						<React.Fragment key={index}>
+							{page.results.map((game) => (
+								<div
+									className={
+										"flex flex-col flex-auto  sm:w-72 sm:max-w-96  rounded-lg overflow-hidden shadow-2xl  m-1 mb-2 "
+									}
+									key={game.id}
+								>
+									<img src={MediaResize(game.background_image)} alt="Image" />
+									<div className="flex flex-col">
+										<div className="flex items-center justify-between px-3 py-2 h-auto">
+											<div>{game.name}</div>
+											<div className="bg-gray-500 opacity-70 rounded p-0 text-center">
+												{game.rating}
+											</div>
+										</div>
+										<div className="px-3 pb-2">
+											<PlatformIcons
+												platforms={game.parent_platforms.map((p) => p.platform)}
+											/>
 										</div>
 									</div>
-									<div className="px-3 pb-2">
-										<PlatformIcons
-											platforms={game.parent_platforms.map((p) => p.platform)}
-										/>
-									</div>
 								</div>
-							</div>
-						))}
-					</React.Fragment>
-				))}
-			</div>
-			<div>
-				<button onClick={() => fetchNextPage()}>
-					{isFetching ? "Loading..." : "More"}
-				</button>
-			</div>
+							))}
+						</React.Fragment>
+					))}
+				</div>
+			</InfiniteScroll>
 		</>
 	);
 };
