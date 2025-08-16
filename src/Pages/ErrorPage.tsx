caee4e40fad784164833fc0ce7e69e7f7aacffcf
Mohammed <GM+mami186@users.noreply.github.com>
2025-08-16 13:39:40 +0300

    fix error handling

--- src/Pages/ErrorPage.tsx
+++ src/Pages/ErrorPage.tsx
@@ -1,12 +1,22 @@
-import { isRouteErrorResponse, useRouteError } from "react-router-dom"
-
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import Navbar from "../Components/Navbar";
 
 const ErrorPage = () => {
-    const error =useRouteError()
-console.log(error)
-    return (
-    <div>{isRouteErrorResponse(error)?'invalid page':'something went wrong'   }</div>
-  )
-}
+	const error = useRouteError();
+	console.log(error);
+	return (
+		<>
+      <Navbar/>
+			<div className="p-15 h-screen">
+				<h1 className="text-7xl font-bold" >Oops....</h1>
+				<div className="text-3xl mt-3  ">
+					{isRouteErrorResponse(error)
+						? "ERROR ... invalid page"
+						: "ERRO ...something went wrong"}
+				</div>
+			</div>
+		</>
+	);
+};
 
-export default ErrorPage
+export default ErrorPage;
